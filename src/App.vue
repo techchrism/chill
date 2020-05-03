0c5af20b5d0fe8c1e1de225214d298e1af39872e
TheTechdoodle <admin+github@darkender.com>
2020-05-03 14:07:30 -0700

    Put media meta in its own file

--- src/App.vue
+++ src/App.vue
@@ -63,6 +63,7 @@
     import Animation from './components/Animation';
     import fscreen from 'fscreen';
     import {mapState} from 'vuex';
+    import {sounds, animations, music} from './files';
 
     export default {
         name: 'App',
@@ -70,57 +71,9 @@
         data()
         {
             return {
-                sounds: [
-                    {name: 'Birds', file: 'birds.webm', element: null},
-                    {name: 'Crickets', file: 'crickets.webm', element: null},
-                    {name: 'Fire', file: 'fire.webm', element: null},
-                    {name: 'Jungle', file: 'jungle.webm', element: null},
-                    {name: 'People', file: 'people.webm', element: null},
-                    {name: 'Rain', file: 'rain.webm', element: null},
-                    {name: 'Seagulls', file: 'seagulls.webm', element: null},
-                    {name: 'Swamp', file: 'swamp.webm', element: null},
-                    {name: 'Thunder', file: 'thunder.webm', element: null},
-                    {name: 'Umbrella Rain', file: 'umbrella-rain.webm', element: null},
-                    {name: 'Waves', file: 'waves.webm', element: null},
-                    {name: 'Wind', file: 'wind.webm', element: null}
-                ],
-                animations: [
-                    {file: '01.gif', source: '1041uuu'},
-                    {file: '02.gif', source: '1041uuu'},
-                    {file: '03.gif', source: 'Mark Ferrari'},
-                    {file: '04.gif', source: 'Mark Ferrari'},
-                    {file: '05.gif', source: 'unknown'},
-                    {file: '06.gif', source: 'Mark Ferrari'},
-                    {file: '07.gif', source: 'Mark Ferrari'},
-                    {file: '08.gif', source: 'Mark Ferrari'},
-                    {file: '09.gif', source: 'Mark Ferrari'},
-                    {file: '10.gif', source: 'Mark Ferrari'},
-                    {file: '11.gif', source: 'Mark Ferrari'},
-                    {file: '12.gif', source: 'Mark Ferrari'},
-                    {file: '13.gif', source: '1041uuu'},
-                    {file: '14.gif', source: '1041uuu'},
-                    {file: '15.gif', source: 'Waneella'},
-                    {file: '16.gif', source: '1041uuu'},
-                    {file: '17.gif', source: 'prevalentirrationality'},
-                    {file: '18.gif', source: 'pixel-city'},
-                    {file: '19.gif', source: 'iSohei'},
-                    {file: '20.gif', source: '1041uuu'},
-                    {file: '21.gif', source: '1041uuu'},
-                    {file: '22.gif', source: '1041uuu'},
-                    {file: '23.gif', source: '1041uuu'},
-                ],
-                music: [
-                    {name: 'END THEME', artist: 'Tenmon', file: 'end_theme.webm'},
-                    {name: 'No More Tears', artist: 'David Nevue', file: 'no_more_tears.webm'},
-                    {name: 'To The Sky', artist: 'Dirk Maassen', file: 'to_the_sky.webm'},
-                    {name: 'River Flows in You', artist: 'Yiruma', file: 'river_flows_in_you.webm'},
-                    {name: 'Kiss the Rain', artist: 'Yiruma', file: 'kiss_the_rain.webm'},
-                    {name: 'May Be', artist: 'Yiruma', file: 'may_be.webm'},
-                    {name: 'Reminiscent', artist: 'Yiruma', file: 'reminiscent.webm'},
-                    {name: 'Loanna', artist: 'Yiruma', file: 'loanna.webm'},
-                    {name: 'The Days That\'ll Never Come', artist: 'Yiruma', file: 'the_days_thatll_never_come.webm'},
-                    {name: 'I Could See You', artist: 'Yiruma', file: 'i_could_see_you.webm'}
-                ],
+                sounds,
+                animations,
+                music,
                 selectedAnimation: {},
                 selectedSong: {},
                 songProgress: 0,
@@ -272,7 +225,7 @@
             },
             keydown(e)
             {
-                switch(e.which)
+                switch(e.keyCode)
                 {
                     case 32: // space
                     {
@@ -299,7 +252,6 @@
                         this.prevSong();
                         break;
                     }
-
                     default: return;
                 }
                 e.preventDefault();
---
+++ src/files.js
@@ -0,0 +1,53 @@
+module.exports.sounds = [
+    {name: 'Birds', file: 'birds.webm'},
+    {name: 'Crickets', file: 'crickets.webm'},
+    {name: 'Fire', file: 'fire.webm'},
+    {name: 'Jungle', file: 'jungle.webm'},
+    {name: 'People', file: 'people.webm'},
+    {name: 'Rain', file: 'rain.webm'},
+    {name: 'Seagulls', file: 'seagulls.webm'},
+    {name: 'Swamp', file: 'swamp.webm'},
+    {name: 'Thunder', file: 'thunder.webm'},
+    {name: 'Umbrella Rain', file: 'umbrella-rain.webm'},
+    {name: 'Waves', file: 'waves.webm'},
+    {name: 'Wind', file: 'wind.webm'}
+];
+
+module.exports.animations = [
+    {file: '01.gif', source: '1041uuu'},
+    {file: '02.gif', source: '1041uuu'},
+    {file: '03.gif', source: 'Mark Ferrari'},
+    {file: '04.gif', source: 'Mark Ferrari'},
+    {file: '05.gif', source: 'unknown'},
+    {file: '06.gif', source: 'Mark Ferrari'},
+    {file: '07.gif', source: 'Mark Ferrari'},
+    {file: '08.gif', source: 'Mark Ferrari'},
+    {file: '09.gif', source: 'Mark Ferrari'},
+    {file: '10.gif', source: 'Mark Ferrari'},
+    {file: '11.gif', source: 'Mark Ferrari'},
+    {file: '12.gif', source: 'Mark Ferrari'},
+    {file: '13.gif', source: '1041uuu'},
+    {file: '14.gif', source: '1041uuu'},
+    {file: '15.gif', source: 'Waneella'},
+    {file: '16.gif', source: '1041uuu'},
+    {file: '17.gif', source: 'prevalentirrationality'},
+    {file: '18.gif', source: 'pixel-city'},
+    {file: '19.gif', source: 'iSohei'},
+    {file: '20.gif', source: '1041uuu'},
+    {file: '21.gif', source: '1041uuu'},
+    {file: '22.gif', source: '1041uuu'},
+    {file: '23.gif', source: '1041uuu'},
+];
+
+module.exports.music = [
+    {name: 'END THEME', artist: 'Tenmon', file: 'end_theme.webm'},
+    {name: 'No More Tears', artist: 'David Nevue', file: 'no_more_tears.webm'},
+    {name: 'To The Sky', artist: 'Dirk Maassen', file: 'to_the_sky.webm'},
+    {name: 'River Flows in You', artist: 'Yiruma', file: 'river_flows_in_you.webm'},
+    {name: 'Kiss the Rain', artist: 'Yiruma', file: 'kiss_the_rain.webm'},
+    {name: 'May Be', artist: 'Yiruma', file: 'may_be.webm'},
+    {name: 'Reminiscent', artist: 'Yiruma', file: 'reminiscent.webm'},
+    {name: 'Loanna', artist: 'Yiruma', file: 'loanna.webm'},
+    {name: 'The Days That\'ll Never Come', artist: 'Yiruma', file: 'the_days_thatll_never_come.webm'},
+    {name: 'I Could See You', artist: 'Yiruma', file: 'i_could_see_you.webm'}
+];
